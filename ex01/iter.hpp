1e5879571bc153ff8e8727f1b268a52a892afc08
cgodecke <cgodecke@student.42wolfsburg.de>
2023-11-29 17:02:13 +0100

    started ex01

---
+++ ex01/iter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 16:42:56 by cgodecke          #+#    #+#             */
+/*   Updated: 2023/11/29 16:53:16 by cgodecke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include<iostream>
+
+template<typename T>
+void iter(T *array_address[], T array_length, T function)
+{
+	
+}
+
+#endif
