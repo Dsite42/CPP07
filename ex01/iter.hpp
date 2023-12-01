9e71756e99cfd5bba9aa41423323e3944b511e10
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-12-01 15:07:37 +0100

    finished ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 16:42:56 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/11/29 16:53:16 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/12/01 14:49:27 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 
 # include<iostream>
 
-template<typename T>
-void iter(T *array_address[], T array_length, T function)
+template<typename T_array, typename T_function>
+void iter(T_array *array, int length, T_function function)
 {
-	
+	for (int i = 0; i < length; i++)
+	{
+		function(array[i]);
+	}
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,25 +6,38 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 16:01:20 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/11/29 17:01:43 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/12/01 15:04:15 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-void times_two(T array)
+void times_two(int &i)
 {
-	
+	i *= 2;
+}
+
+void to_upper(std::string &str)
+{
+	for (size_t i = 0; i < str.length(); i++)
+		str[i] = std::toupper(str[i]);
 }
 
 int main( void )
 {
+	std::cout << "\033[33m" << "Test int array\n"<< "\033[0m";
 	int int_arr[] = {1, 2, 3};
 
+	::iter(int_arr, 3, times_two);
+	for (int i = 0; i < 3; i++)
+		std::cout << int_arr[i] << std::endl;
 
+	std::cout << "\033[33m" << "Test string array\n"<< "\033[0m";
+	std::string str_arr[] = {"Hello", "World", "!"};
+	::iter(str_arr, 3, to_upper);
+	for (int i = 0; i < 3; i++)
+		std::cout << str_arr[i] << std::endl;
 
-	std::cout << int_arr[0];
-	::iter(*int_arr, 3, times_two);
 
 	return 0;
 }
